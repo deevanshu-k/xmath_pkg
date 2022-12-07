0dec612b8a101c0e503fb7d6a94b2fe61ee2a138
Deevanshu <deevanshukushwah80@gmail.com>
2022-12-08 01:07:45 +0530

    Build ready for new pow fnx

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,3 @@
 export declare function multiply(...args: number[]): number;
 export declare function add(...args: number[]): number;
+export declare function pow(base: number, power: number): number;
